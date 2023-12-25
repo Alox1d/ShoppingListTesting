philipplackner/ShoppingListTestingYT

Added ImageAdapter.kt and tests for ImagePickFragment.kt

app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/repositories/FakeShoppingRepositoryAndroidTest.kt
@@ -1,0 +1,67 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+
+class FakeShoppingRepositoryAndroidTest : ShoppingRepository {
+
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+
+    private val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return if(shouldReturnNetworkError) {
+            Resource.error("Error", null)
+        } else {
+            Resource.success(ImageResponse(listOf(), 0, 0))
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+

Alox1d/ShoppingListTesting

app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/repositories/FakeShoppingRepositoryAndroidTest.kt
@@ -1,0 +1,67 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.utils.Resource
+
+class FakeShoppingRepositoryAndroidTest : ShoppingRepository {
+
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+
+    private val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return if(shouldReturnNetworkError) {
+            Resource.error("Error", null)
+        } else {
+            Resource.success(ImageResponse(listOf(), 0, 0))
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+